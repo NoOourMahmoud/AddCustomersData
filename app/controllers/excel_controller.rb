644d88bb566e91686439a61232d5b3fc2a01ad1c
NoOourMahmoud <eng.nourhanmahmoud@yahoo.com>
2021-12-09 20:18:06 +0200

    Data entry job

--- app/controllers/excel_controller.rb
+++ app/controllers/excel_controller.rb
@@ -3,30 +3,7 @@ class ExcelController < ApplicationController
     end
 
     def uploadSheet
-        workbook = RubyXL::Parser.parse(ActiveStorage::Blob.service.path_for(ActiveStorage::Blob.find_by(id: 1).key))
-
-		#Acquire WorkSheet
-		worksheet = workbook[0]
-        @cell = worksheet[0][0]
-
-        print(worksheet[0][0])
-
-		customarData = []
-
-		worksheet.each { |row|
-			customarData = []
-		   	row.cells.each { |cell|
-				val = cell.value
-				#print(val, '  ->   ')
-				customarData.push(val)
-
-		   	}
-		   newCustomar = Customer.new(mobile: customarData[0], name: customarData[1], address: customarData[2])
-		   newCustomar.save
-
-		   #print("\n")
-		   #print(customarData, "\n")
-		}
+        CustomerDataEntryJob.perform_later params
         redirect_to root_path
     end
 end
---
+++ app/jobs/customer_data_entry_job.rb
@@ -0,0 +1,31 @@
+class CustomerDataEntryJob < ApplicationJob
+  queue_as :default
+
+  def perform(params)
+    # Do something later
+    workbook = RubyXL::Parser.parse(ActiveStorage::Blob.service.path_for(ActiveStorage::Blob.find_by(id: 4).key))
+
+		#Acquire WorkSheet
+		worksheet = workbook[0]
+        @cell = worksheet[0][0]
+
+        print(worksheet[0][0])
+
+		customarData = []
+
+		worksheet.each { |row|
+			customarData = []
+		   	row.cells.each { |cell|
+				val = cell.value
+				#print(val, '  ->   ')
+				customarData.push(val)
+
+		   	}
+		   newCustomar = Customer.new(mobile: customarData[0], name: customarData[1], address: customarData[2])
+		   newCustomar.save
+
+		   #print("\n")
+		   #print(customarData, "\n")
+		}
+  end
+end
