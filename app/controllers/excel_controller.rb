db18f59747ea56dd546f47ff00584b2121ba08ae
NoOourMahmoud <eng.nourhanmahmoud@yahoo.com>
2021-12-12 11:16:42 +0200

    update

--- app/controllers/excel_controller.rb
+++ app/controllers/excel_controller.rb
@@ -3,7 +3,30 @@ class ExcelController < ApplicationController
     end
 
     def uploadSheet
-        CustomerDataEntryJob.perform_later params[:attachment]
+    CustomerDataEntryJob.perform_later params[:attachment]
+    #workbook = RubyXL::Parser.parse(ActiveStorage::Blob.service.path_for(ActiveStorage::Blob.find_by(id: 1).key))
+	#workbook = RubyXL::Parser.parse(params[:attachment][])
+
+	#Acquire WorkSheet
+	worksheet = workbook[0]
+	@cell = worksheet[0][0]
+
+	print(worksheet[0][0])
+
+	customarData = []
+
+	worksheet.each { |row|
+		customarData = []
+		row.cells.each { |cell|
+			val = cell.value
+			#print(val, '  ->   ')
+			customarData.push(val)
+
+		}
+		newCustomar = Customer.new(mobile: customarData[0], name: customarData[1], address: customarData[2])
+		newCustomar.save
+
+        
         redirect_to root_path
     end
 end
--- app/jobs/customer_data_entry_job.rb
+++ app/jobs/customer_data_entry_job.rb
@@ -1,31 +1,32 @@
 class CustomerDataEntryJob < ApplicationJob
   queue_as :default
 
-  def perform(params)
+  def perform(attached)
     # Do something later
     workbook = RubyXL::Parser.parse(ActiveStorage::Blob.service.path_for(ActiveStorage::Blob.find_by(id: 1).key))
+	#workbook = RubyXL::Parser.parse(attached[:file])
 
-		#Acquire WorkSheet
-		worksheet = workbook[0]
-        @cell = worksheet[0][0]
+	#Acquire WorkSheet
+	worksheet = workbook[0]
+	@cell = worksheet[0][0]
 
-        print(worksheet[0][0])
+	print(worksheet[0][0])
 
+	customarData = []
+
+	worksheet.each { |row|
 		customarData = []
+		row.cells.each { |cell|
+			val = cell.value
+			#print(val, '  ->   ')
+			customarData.push(val)
 
-		worksheet.each { |row|
-			customarData = []
-		   	row.cells.each { |cell|
-				val = cell.value
-				#print(val, '  ->   ')
-				customarData.push(val)
-
-		   	}
-		   newCustomar = Customer.new(mobile: customarData[0], name: customarData[1], address: customarData[2])
-		   newCustomar.save
-
-		   #print("\n")
-		   #print(customarData, "\n")
 		}
+		newCustomar = Customer.new(mobile: customarData[0], name: customarData[1], address: customarData[2])
+		newCustomar.save
+
+		#print("\n")
+		#print(customarData, "\n")
+	}
   end
 end
