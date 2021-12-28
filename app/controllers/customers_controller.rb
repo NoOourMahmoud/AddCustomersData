1ff1429571944a7e4d84f2d749298e2d06916fa7
NoOourMahmoud <eng.nourhanmahmoud@yahoo.com>
2021-12-28 21:13:31 +0200

    Add Edit and Delete

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,36 @@
+class CustomersController < ApplicationController
+  before_action :setCustomer, only: [:destroy, :edit]
+
+  def index
+    @pagy, @customers = pagy(Customer.all, items: 7)
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to customers_path
+  end
+
+  def edit
+    $prevId = @customer[:id]
+  end
+
+  def update
+    @customer = Customer.find($prevId)
+    if @customer.update(params_customer)
+      redirect_to customers_path, notice: "customer updated"
+    else
+      render :edit
+    end
+  end
+  
+
+  private
+  #there isn't ID in params
+  def setCustomer
+    @customer = Customer.find(params[:format])
+  end
+
+  def params_customer
+    params.require(:customer).permit(:mobile, :name, :address)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
   root to: "excel#newSheet"
   post "excel", to: "excel#uploadSheet"
 
+  #for Json API
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index, :show]
     end
   end
+
+  #for pagination with editing ability
+  get "customers", to: "customers#index"
+  get "customers/edit", to: "customers#edit"
+  patch "customers/edit", to: "customers#update"
+  delete "customers", to: "customers#destroy"
 end
