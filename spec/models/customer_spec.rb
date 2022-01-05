9b53dcc7ed3c2cbf020e55f6f47898a88c7780be
NoOourMahmoud <eng.nourhanmahmoud@yahoo.com>
2022-01-05 21:25:26 +0200

    Do customer validation tests with shoulda-matchers

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,26 +1,10 @@
 require "rails_helper"
 
 describe Customer, type: :model do
-  subject { 
-    described_class.new(
-      mobile: "20 1176987456",
-      name: "Nourhan Mahmoud",
-      address: "7 oraby st cairo"
-    )
-   } 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
+  describe "Validations" do
+    it { should validate_presence_of(:mobile) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_uniqueness_of(:mobile)}
   end
-  it "isn't valid without Mobile" do
-    subject.mobile = nil
-    expect(subject).not_to be_valid
-  end
-  it "isn't valid without Name" do
-    subject.name = nil
-    expect(subject).not_to be_valid
-  end
-  it "isn't valid without Address" do
-    subject.address = nil
-    expect(subject).not_to be_valid
-  end  
 end
